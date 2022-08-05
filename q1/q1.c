58c8633e4aa9b0b5654853bded27f4b77cba49b6
VanSHOE <rahulgargofficial@gmail.com>
2022-08-06 01:42:22 +0530

    q1 done

--- q1/q1.c
+++ q1/q1.c
@@ -2,12 +2,21 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
 
-void main()
+void main(int argc, char **argv)
 {
+    if (argc != 2)
+    {
+        printf("Usage: %s <file>\n", argv[0]);
+        return;
+    }
     char buff[1];
-    int input = open("input.txt", O_RDONLY);
-    int output = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    int input = open(argv[1], O_RDONLY);
+    mkdir("./Assignment", 0777);
+
+    int output = open("./Assignment/output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0777);
 
     int inputLength = 0;
 
@@ -15,7 +24,7 @@ void main()
     {
         inputLength++;
     }
-    // Reverse file
+
     int pos = -1;
     lseek(input, pos--, SEEK_END);
     int outputLength = 0;
@@ -23,9 +32,13 @@ void main()
     {
         write(output, buff, 1);
         outputLength++;
+        double progress = (double)outputLength * 100 / (double)inputLength;
+        char progressString[100];
+        sprintf(progressString, "\r%.2f%%", progress);
+        write(STDOUT_FILENO, progressString, strlen(progressString));
         lseek(input, pos--, SEEK_END);
     }
-
+    write(STDOUT_FILENO, "\n", 1);
     close(input);
     close(output);
 }
