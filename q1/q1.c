fc7cc1bde9d54b78d56872fbf898a668352debef
VanSHOE <rahulgargofficial@gmail.com>
2022-08-06 01:25:36 +0530

    Progress on Q1

--- q1/q1.c
+++ q1/q1.c
@@ -5,5 +5,27 @@
 
 void main()
 {
-    lseek(0, 0, 0);
+    char buff[1];
+    int input = open("input.txt", O_RDONLY);
+    int output = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+
+    int inputLength = 0;
+
+    while (read(input, buff, 1) > 0)
+    {
+        inputLength++;
+    }
+    // Reverse file
+    int pos = -1;
+    lseek(input, pos--, SEEK_END);
+    int outputLength = 0;
+    while (read(input, buff, 1) > 0 && outputLength < inputLength)
+    {
+        write(output, buff, 1);
+        outputLength++;
+        lseek(input, pos--, SEEK_END);
+    }
+
+    close(input);
+    close(output);
 }
